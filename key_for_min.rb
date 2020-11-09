6f15ff3c62862cee70915c237ec6615f66059699
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-09 19:16:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,5 +12,5 @@ def key_for_min_value(name_hash)
 end
 
 def select_winner(passengers)
- passengers.each {|key, value| return key if value == passengers.values.max
+ passengers.each {|key, value| return key if value == passengers.values.max}
 end
