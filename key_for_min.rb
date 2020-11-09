40fa02579a1203dbdc623b364ada4b7da47f9040
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-09 19:14:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,13 +12,5 @@ def key_for_min_value(name_hash)
 end
 
 def select_winner(passengers)
-  winner = ""
-  passengers.each do |key, value|
-    
-    if key == :suite_a && value.start_with?("A")
-      
-      winner = value
-    end
-  end
-  winner
+ 
 end
