294c90b9aaf0675f7154c6dce3be5d8ff225cfd5
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-09 19:36:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -16,3 +16,13 @@ def key_for_min_value(hash)
  lowest_key
 end
 
+def key_for_min_value(name_hash)
+  lowest_key = name_hash.reduce do |key, value|
+    key.last > value.last ? value : key
+  end
+    if name_hash == {}
+      lowest_key
+    else
+      lowest_key.first
+    end
+end
